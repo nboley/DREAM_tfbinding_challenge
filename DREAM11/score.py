a53f17d43d8c3b030a016940b4523a9abaf52d57
Nathan Boley <npboley@gmail.com>
2016-07-11 17:32:11 -0700

    Cleaned upt he test infrastructure.

--- DREAM11/score.py
+++ DREAM11/score.py
@@ -3,12 +3,18 @@ import re
 from itertools import izip_longest
 import random
 import math
+from bisect import bisect
+from collections import namedtuple
 
 import numpy as np
-from scipy.stats import itemfreq
+from scipy.stats import itemfreq, norm
 
 from pyDNAbinding.misc import optional_gzip_open
 from pyTFbindtools.cross_validation import ClassificationResult
+
+# which measures to use to evaluate teh overall score
+MEASURE_NAMES = ['recall_at_10_fdr', 'recall_at_50_fdr', 'auROC', 'auPRC']
+ValidationResults = namedtuple('ValidationResults', MEASURE_NAMES)
 
 def build_sample_test_file(truth_fname, score_column_index, output_fname):
     ofp = open(output_fname, "w")
@@ -16,7 +22,7 @@ def build_sample_test_file(truth_fname, score_column_index, output_fname):
         for i, line in enumerate(fp):
             # skip the header
             if i == 0: continue
-            if i%100000 == 0: print i
+            if i%1000000 == 0: print "Finished processing line", i
             data = line.split()
             region = data[:3]
             label = data[score_column_index]
@@ -81,8 +87,8 @@ def verify_file_and_build_scores_array(truth_fname, submitted_fname):
             assert False, "Unrecognized label '%s'" % label
     return np.array(labels), np.array(scores)
 
-def iter_block_samples(labels, scores, num_splits=20):
-    block_indices = range(0, len(labels), len(labels)/20)
+def iter_block_samples(labels, scores, num_splits=10):
+    block_indices = range(0, len(labels), len(labels)/num_splits)
     slices = [slice(*x) for x in zip(block_indices[:-1], block_indices[1:])]
     # leave one slice out
     for slice_index_to_skip in xrange(len(slices)):
@@ -98,19 +104,11 @@ def iter_block_samples(labels, scores, num_splits=20):
     return
 
 def empirical_p_value(score, other_scores):
-    rank = max(i for i, ref_score in enumerate(sorted(other_scores))
-               if score > ref_score)
-    return max(0.5, float(rank)/len(other_scores+1))
+    other_scores.sort()
+    rank = bisect(other_scores, score)
+    return max(0.5, float(rank)/(len(other_scores)+1))
 
-def verify_and_score_submission(ref_fname, submitted_fname):
-    # find the existing scores
-    recalls_at_10_fdr = [0.654, 0.652, 0.651, 0.694, 0.624]
-    recalls_at_50_fdr = [0.662, 0.662, 0.660, 0.672, 0.663]
-    auPRCs = [0.679, 0.703, 0.697, 0.701, 0.702]
-    
-    # build the label and score matrices
-    labels, scores = verify_file_and_build_scores_array(
-        ref_fname, submitted_fname)
+def calc_score_improved_pvalue(labels, scores, previous_scores):
     # bootstrap the validation scores
     results = []
     for i, (ss_labels, ss_scores) in enumerate(
@@ -118,23 +116,75 @@ def verify_and_score_submission(ref_fname, submitted_fname):
         results.append(
             ClassificationResult(ss_labels, ss_scores.round(), ss_scores)
         )
-        
-    # calculate the overall score
-    re_at_fdr_10p_epvs = [
-        empirical_p_value(x.recall_at_10_fdr, recalls_at_10_fdr)
-        for x in results]
-    re_at_fdr_50p_epvs = [
-        empirical_p_value(x.recall_at_50_fdr, recalls_at_50_fdr)
-        for x in results]
-    auPRC_epvs = [empirical_p_value(x.auPRC, auPRCs) for x in results]
-    scores = []
-    for data in zip(auPRC_epvs, re_at_fdr_50p_epvs, re_at_fdr_10p_epvs):
-        scores.append(sum(-math.log(x) for x in data))
-    return scores
+        print results[-1]
+
+    p_values = []
+    for measure_name in MEASURE_NAMES:
+        # find the mean and sd for all of the bootstrapped score estimates
+        measures = np.array([getattr(x, measure_name) for x in results])
+        mean, std = measures.mean(), measures.std()
+        p_val = norm.cdf(getattr(previous_scores, measure_name), mean, std)
+        # make the bonferroni correction
+        p_values.append(min(1.0, 4*p_val))
+    print p_values
+    return min(p_values)
+
+def build_combined_score(results, others_results):
+    p_values = []
+    for measure_name in MEASURE_NAMES:
+        p_value = empirical_p_value(
+            getattr(results, measure_name),
+            [getattr(x, measure_name) for x in others_results]
+        )
+        p_values.append(p_value)
+    return sum(-math.log10(x) for x in p_values)
+
+def verify_and_score_submission(
+        ref_fname, submitted_fname, previous_scores, other_participants_results):
+    # build the label and score matrices
+    labels, scores = verify_file_and_build_scores_array(
+        ref_fname, submitted_fname)
+
+    # decide if the score is better than previous scores
+    p_val = calc_score_improved_pvalue(labels, scores, previous_scores)
+    full_results = ClassificationResult(labels, scores.round(), scores)
+
+    # compute the measures on the full submitted scores
+    results = ValidationResults(*[
+        getattr(full_results, attr_name) for attr_name in MEASURE_NAMES])
+
+    # compute the combined score
+    combined_score = build_combined_score(results, other_participants_results)
+    
+    return combined_score, p_val, results
+
+def build_test_data():
+    recalls_at_10_fdr = [0.654, 0.652, 0.651, 0.694, 0.624]
+    recalls_at_50_fdr = [0.662, 0.662, 0.660, 0.672, 0.663]
+    auPRCs = [0.679, 0.703, 0.697, 0.701, 0.702]
+    auROCs = [0.978, 0.979, 0.982, 0.978, 0.979]
+
+    other_participants_results = [
+        ValidationResults(*x) for x in zip(
+            recalls_at_10_fdr, recalls_at_50_fdr, auPRCs, auROCs)
+    ]
+    
+    previous_scores = ValidationResults(
+        random.choice(recalls_at_10_fdr),
+        random.choice(recalls_at_50_fdr),
+        random.choice(auROCs),
+        random.choice(auPRCs)
+    )
+
+    return other_participants_results, previous_scores
 
 def main():
-    #build_sample_test_file(sys.argv[1], 3, "test.scores")
-    res = verify_and_score_submission(sys.argv[1], sys.argv[2])
+    other_participants_results, previous_scores = build_test_data()
+    labels_file = sys.argv[1]
+    build_sample_test_file(labels_file, 3, "test.scores")
+    submission_file = "test.scores"
+    res = verify_and_score_submission(
+        labels_file, submission_file, previous_scores, other_participants_results)
     print res
 
 main()
